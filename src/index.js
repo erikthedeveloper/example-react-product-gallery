5b5859406cd61afa721d18955029490db1a54f49
Erik Aybar <erikthedeveloper@gmail.com>
2017-08-28 16:16:48 -0600

    Add normalize.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/normalize.css/normalize.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
