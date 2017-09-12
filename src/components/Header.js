75f33dfdd84d05fa5ba25966efe8fe8fcf79ca0d
Erik Aybar <erikthedeveloper@gmail.com>
2017-09-12 10:52:13 -0600

    Link header logo and update text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Header.css';
 import {SearchInput} from './SearchInput';
+import {Link} from 'react-router-dom';
 
 export const Header = ({searchText, setSearchText}) => (
   <div className="Header">
     <div className="content header-flex">
-      <div className="Header__title">
-        Amazing<br />
-        Store
-      </div>
+      <Link to="/" className="Header__title">
+        Robots<br />
+        &amp; Kittens
+      </Link>
       <SearchInput searchText={searchText} setSearchText={setSearchText} />
     </div>
   </div>
