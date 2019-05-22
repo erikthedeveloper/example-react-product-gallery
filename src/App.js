a331804568e4a9c8518a0a40be05bec632bd12d6
Erik Aybar <erikthedeveloper@gmail.com>
2019-05-22 06:44:08 -0600

    Add example usages of requests to starter App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import './App.css';
 import logo from './logo.svg';
+import * as requests from './requests';
 
 export default function App() {
+  // Here as an example to get you started with requests.js
+  React.useEffect(() => {
+    (async () => {
+      const categories = await requests.getCategories();
+      const products = await requests.getProducts({
+        categoryId: categories[0].id,
+      });
+      const product = await requests.getProduct(products[0].id);
+      console.log('Example request: categories', categories);
+      console.log('Example request: products', products);
+      console.log('Example request: product', product);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
