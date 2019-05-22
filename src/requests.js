9cec98bf77a69a15eceb689ce90385c986c92485
Erik Aybar <erikthedeveloper@gmail.com>
2019-05-22 06:37:21 -0600

    Rearrange requests.js

--- src/requests.js
+++ src/requests.js
@@ -1,31 +1,5 @@
 import * as data from './data';
 
-// Various filters for filtering down products
-const filterCategory = id => ({categoryId}) => categoryId === id;
-
-const filterMinPrice = minPrice => ({price}) => price >= minPrice;
-
-const filterMaxPrice = maxPrice => ({price}) => price <= maxPrice;
-
-const filterSearchText = searchText => ({name}) =>
-  name.toLowerCase().includes(searchText.toLowerCase());
-
-/**
- * Semi-random delay MS for "requests".
- * @return Number
- */
-const requestDelay = () => Math.max(250, Math.ceil(Math.random() * 1500));
-
-const noopTrue = () => true;
-
-/**
- * Find an item by id
- * @param {{id: Number}[]} items
- * @param {Number} id
- * @return Object|null
- */
-const findById = (items, id) => items.find(item => item.id === id);
-
 /**
  * "Request" products given query params
  * @param activeCategoryId
@@ -62,7 +36,7 @@ export function getProducts({
  */
 export function getProduct(id) {
   return new Promise(resolve => {
-    const item = findById(data.products, id);
+    const item = data.products.find(item => item.id === id);
     setTimeout(resolve.bind(null, item), requestDelay());
   });
 }
@@ -76,3 +50,21 @@ export function getCategories() {
     setTimeout(resolve.bind(null, data.categories), requestDelay());
   });
 }
+
+// Various filters for filtering down products
+const filterCategory = id => ({categoryId}) => categoryId === id;
+
+const filterMinPrice = minPrice => ({price}) => price >= minPrice;
+
+const filterMaxPrice = maxPrice => ({price}) => price <= maxPrice;
+
+const filterSearchText = searchText => ({name}) =>
+  name.toLowerCase().includes(searchText.toLowerCase());
+
+/**
+ * Semi-random delay MS for "requests".
+ * @return Number
+ */
+const requestDelay = () => Math.max(250, Math.ceil(Math.random() * 1500));
+
+const noopTrue = () => true;
